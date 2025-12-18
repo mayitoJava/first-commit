735246f57d92543c193af489c6bf4bddc824bade
mariovilchis <mariovilchis01@hotmail.com>
2025-12-18 13:00:47 -0600

    Calculadorapostman/updatedireccion

--- src/main/java/AVilchis/ProgramacionNCapasNoviembre25/DAO/DireccionJPADAOImplementation.java
+++ src/main/java/AVilchis/ProgramacionNCapasNoviembre25/DAO/DireccionJPADAOImplementation.java
@@ -1,4 +1,3 @@
-
 package AVilchis.ProgramacionNCapasNoviembre25.DAO;
 
 import AVilchis.ProgramacionNCapasNoviembre25.JPA.direccionJPA;
@@ -7,28 +6,51 @@ import jakarta.persistence.EntityManager;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public class DireccionJPADAOImplementation implements IDireccionJPA{
-    
+public class DireccionJPADAOImplementation implements IDireccionJPA {
+
     @Autowired
     private EntityManager entityManager;
 
     @Override
     public Result GetById(int IdDireccion) {
         Result result = new Result();
-        
-        direccionJPA direccionBD = entityManager.find(direccionJPA.class,IdDireccion);
-        
+
+        direccionJPA direccionBD = entityManager.find(direccionJPA.class, IdDireccion);
+
         ModelMapper modelMaperr = new ModelMapper();
-        
+
         AVilchis.ProgramacionNCapasNoviembre25.ML.Direccion direccionML = modelMaperr.map(direccionBD, AVilchis.ProgramacionNCapasNoviembre25.ML.Direccion.class);
-        
+
         result.Object = direccionML;
-        
+
         return result;
     }
 
-    
-    
+    @Transactional
+    @Override
+    public Result Update(AVilchis.ProgramacionNCapasNoviembre25.ML.Direccion direccion) {
+        Result result = new Result();
+        try {
+            // Verificar si existe en la base de datos
+            direccionJPA direccionDb = entityManager.find(direccionJPA.class, direccion.getIdDireccion());
+
+            if (direccionDb != null) {
+                ModelMapper modelMapper = new ModelMapper();
+                direccionJPA direccionJPA = modelMapper.map(direccion, direccionJPA.class);
+                entityManager.merge(direccionJPA);
+            }
+
+            result.Correct = true;
+
+        } catch (Exception ex) {
+            result.Correct = false;
+            result.ErrorMessage = ex.getLocalizedMessage();
+            result.ex = ex;
+        }
+        return result;
+    }
+
 }
--- src/main/java/AVilchis/ProgramacionNCapasNoviembre25/DAO/IDireccionJPA.java
+++ src/main/java/AVilchis/ProgramacionNCapasNoviembre25/DAO/IDireccionJPA.java
@@ -5,4 +5,5 @@ import AVilchis.ProgramacionNCapasNoviembre25.ML.Result;
 
 public interface IDireccionJPA {
     public Result GetById(int IdDireccion);
+    public Result Update(AVilchis.ProgramacionNCapasNoviembre25.ML.Direccion direccion);
 }
